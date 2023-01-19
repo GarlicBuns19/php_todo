809b14e29037201c9673d41ce61d7d6e63828019
GarlicBuns19 <planterx26@gmail.com>
2023-01-19 18:03:24 +0200

    Add pico css

--- index.php
+++ index.php
@@ -23,12 +23,10 @@ if (isset($_POST['name'])) {
 
     if (empty($name)) {
         echo 'Please enter something todo';
-
     } else {
         if (mysqli_query($conn, $sql)) {
             echo 'Record added';
             header('Location: index.php');
-
         } else {
             // err
             echo 'query error:' . mysqli_error($conn);
@@ -45,7 +43,6 @@ if (isset($_POST['delete'])) {
     if (mysqli_query($conn, $sql)) {
         echo 'Record deleted';
         header('Location: index.php');
-
     } else {
         // err
         echo 'query error:' . mysqli_error($conn);
@@ -59,16 +56,14 @@ if (isset($_POST['edit'])) {
 
     if (empty($edit)) {
         echo 'Please enter a the todo you want to edit';
-
     } else {
 
         $id = $_POST['id_to_edit'];
         $sql = "UPDATE todo_list SET todo_name = '$edit' WHERE todo_id = $id";
 
-        if (mysqli_query($conn, $sql)) {    
+        if (mysqli_query($conn, $sql)) {
             echo 'Record edited';
             header('Location: index.php');
-
         } else {
             echo 'query error:' . mysqli_error($conn);
         }
@@ -84,43 +79,58 @@ if (isset($_POST['edit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php todo</title>
+    <link rel="stylesheet" href="css/pico.min.css">
 </head>
 
 <body>
-    <center>
-        <h1>
-            <?php echo $title; ?>
-        </h1>
+    <h1>
+        <?php echo $title; ?>
+    </h1>
+    <div class="container">
+        <center>
+            <!-- Form to add to array -->
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                <h2>What do you want to do ?</h2>
+                <div class="grid">
+                    <div></div>
+                    <div>
+                        <input type="text" name="name" value="<?php echo htmlspecialchars($name); ?>">
+                        <button type="submit" name="submit" class="outline">Add</button>
+                    </div>
+                    <div></div>
+                </div>
+            </form>
+        </center>
+        <center>
 
-        <!-- ADD form -->
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-            <h2>What do you want to do ?</h2>
-            <input type="text" name="name" value="<?php echo htmlspecialchars($name); ?>">
-            <button type="submit" name="submit">Add</button>
-        </form>
-
+        </center>
         <!-- The Todos array -->
         <?php foreach ($todos as $todo) { ?>
-            <h2>
-                <?php echo htmlspecialchars($todo['todo_name']); ?>
-                <?php echo htmlspecialchars($todo['todo_id']); ?>
-
-                <!-- Delete -->
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                    <input type="hidden" name="id_to_del" value="<?php echo $todo['todo_id']; ?>">
-                    <button type="submit" name="delete">Delete</button>
-                </form>
-
-                <!-- Edit -->
+            <div>
+                <h3>
+                    <?php echo htmlspecialchars($todo['todo_name']); ?>
+                    <?php echo htmlspecialchars($todo['todo_id']); ?>
+                </h3>
+                <!-- Form to Edit Todo -->
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                     <input type="text" name="name_to_edit" value="<?php echo $todo['todo_name']; ?>">
                     <input type="hidden" name="id_to_edit" value="<?php echo $todo['todo_id']; ?>">
-                    <button type="submit" name="edit">Edit</button>
+                    <div class="grid">
+                        <div></div>
+                        <div>
+                            <button type="submit" name="edit">Edit</button>
+                            <!-- Form to Delete Todo-->
+                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                                <input type="hidden" name="id_to_del" value="<?php echo $todo['todo_id']; ?>">
+                                <button type="submit" name="delete">Delete</button>
+                            </form>
+                        </div>
+                        <div></div>
+                    </div>
                 </form>
-            </h2>
-        <?php } ?>
-
-    </center>
+            </div>
+            <?php } ?>
+    </div>
 </body>
 
 </html>
